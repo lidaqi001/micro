d4b47557e79bdb9275692e0b3e29b40b7dc87e28
lidaqi <lidaqi962464@qq.com>
2021-04-22 17:07:17 +0800

    init

---
+++ Common/wrapper/breaker/hystrix/hystrix.go
@@ -0,0 +1,58 @@
+package hystrix
+
+import (
+	"github.com/afex/hystrix-go/hystrix"
+	"github.com/asim/go-micro/v3/client"
+	"github.com/eapache/go-resiliency/retrier"
+	"net/http"
+	"context"
+	"time"
+	"log"
+	"net"
+)
+
+type clientWrapper struct {
+	client.Client
+}
+
+func (c *clientWrapper) Call(ctx context.Context, req client.Request, rsp interface{}, opts ...client.CallOption) error {
+	return hystrix.Do(req.Service()+"."+req.Endpoint(), func() error {
+		// 初始化retrier，每隔100ms重试一次，总共重试3次
+		r := retrier.New(retrier.ConstantBackoff(3, 100*time.Millisecond), nil)
+		// retrier 工作模式和 hystrix 类似，在 Run 方法中将待执行的业务逻辑封装到匿名函数传入即可
+		err := r.Run(func() error {
+			return c.Client.Call(ctx, req, rsp, opts...)
+		})
+		return err
+	}, func(err error) error {
+		// 你可以在这里自定义更复杂的服务降级逻辑作为服务熔断的兜底
+		log.Printf("hystrix fallback error: %v", err)
+		return err
+	})
+}
+
+// NewClientWrapper returns a hystrix client Wrapper.
+func NewClientWrapper() client.Wrapper {
+	return func(c client.Client) client.Client {
+		return &clientWrapper{c}
+	}
+}
+
+func Configure(names []string) {
+	// Hystrix 有默认的参数配置，这里可以针对某些 API 进行自定义配置
+	config := hystrix.CommandConfig{
+		Timeout:               1000,
+		MaxConcurrentRequests: 100,
+		ErrorPercentThreshold: 25,
+	}
+	configs := make(map[string]hystrix.CommandConfig)
+	for _, name := range names {
+		configs[name] = config
+	}
+	hystrix.Configure(configs)
+
+	// 结合 Hystrix Dashboard 将服务状态信息可视化
+	hystrixStreamHandler := hystrix.NewStreamHandler()
+	hystrixStreamHandler.Start()
+	go http.ListenAndServe(net.JoinHostPort("", "88"), hystrixStreamHandler)
+}
---
+++ go.mod
@@ -0,0 +1,18 @@
+module sxx-go-micro
+
+go 1.16
+
+require (
+	github.com/afex/hystrix-go v0.0.0-20180502004556-fa1af6a1f4f5
+	github.com/asim/go-micro/plugins/client/grpc/v3 v3.0.0-20210416163442-a91d1f7a3dbb
+	github.com/asim/go-micro/plugins/registry/consul/v3 v3.0.0-20210416163442-a91d1f7a3dbb
+	github.com/asim/go-micro/plugins/registry/etcd/v3 v3.0.0-20210416163442-a91d1f7a3dbb
+	github.com/asim/go-micro/plugins/server/grpc/v3 v3.0.0-20210416163442-a91d1f7a3dbb
+	github.com/asim/go-micro/plugins/wrapper/ratelimiter/ratelimit/v3 v3.0.0-20210416163442-a91d1f7a3dbb
+	github.com/asim/go-micro/v3 v3.5.1
+	github.com/eapache/go-resiliency v1.1.0
+	github.com/golang/protobuf v1.5.2
+	github.com/juju/ratelimit v1.0.2-0.20191002062651-f60b32039441
+	google.golang.org/grpc/examples v0.0.0-20210422002751-6f35bbbfb82d // indirect
+	google.golang.org/protobuf v1.26.0
+)
